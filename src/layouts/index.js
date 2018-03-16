09077977724a16e84d2cc80ce116af95bd54c429
avi123 <intern.avinash.sharma@redcarpet.com>
2018-03-17 01:40:05 +0530

    shopifyclient

---
+++ src/layouts/index.js
@@ -0,0 +1,207 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Link from 'gatsby-link'
+import Helmet from 'react-helmet'
+import styled, { injectGlobal } from 'styled-components'
+import 'normalize.css'
+import './app.css'
+import ApolloClient from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloLink, concat } from 'apollo-link';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+// importing fonts
+import NeueHaasGrotTextRoman_eot from "../assets/fonts/NeueHaasGrotText-55Roman-Web.eot"
+import NeueHaasGrotTextRoman_woff from "../assets/fonts/NeueHaasGrotText-55Roman-Web.woff"
+import NeueHaasGrotTextRoman_woff2 from "../assets/fonts/NeueHaasGrotText-55Roman-Web.woff2"
+import NeueHaasGrotTextItalic_eot from "../assets/fonts/NeueHaasGrotText-56Italic-Web.eot"
+import NeueHaasGrotTextItalic_woff from "../assets/fonts/NeueHaasGrotText-56Italic-Web.woff"
+import NeueHaasGrotTextItalic_woff2 from "../assets/fonts/NeueHaasGrotText-56Italic-Web.woff2"
+import NeueHaasGrotDispRoman_eot from "../assets/fonts/NeueHaasGrotDisp-55Roman-Web.eot"
+import NeueHaasGrotDispRoman_woff from "../assets/fonts/NeueHaasGrotDisp-55Roman-Web.woff"
+import NeueHaasGrotDispRoman_woff2 from "../assets/fonts/NeueHaasGrotDisp-55Roman-Web.woff2"
+import NeueHaasGrotDispItalic_eot from "../assets/fonts/NeueHaasGrotDisp-56Italic-Web.eot"
+import NeueHaasGrotDispItalic_woff from "../assets/fonts/NeueHaasGrotDisp-56Italic-Web.woff"
+import NeueHaasGrotDispItalic_woff2 from "../assets/fonts/NeueHaasGrotDisp-56Italic-Web.woff2"
+
+
+
+// const httpLink = new HttpLink({ uri: 'https://huntings-cafe.myshopify.com/api/graphql' });
+const httpLink = new HttpLink({ uri: 'https://avi-shop-easy-shopping.myshopify.com/api/graphql' });
+
+const authMiddleware  = new ApolloLink((operation, forward) => {
+  operation.setContext({
+    headers: {
+      'X-Shopify-Storefront-Access-Token': 'c5a309acd174a3624e7f50eebee41b3b'
+    }
+  });
+
+  return forward(operation);
+})
+
+const client = new ApolloClient({
+  link: concat(authMiddleware, httpLink),
+  cache: new InMemoryCache()
+});
+
+
+
+/*
+  Layout File
+  this file is the base of every page
+  useful for headers & navs
+
+  this is where global styles/font loading lives
+*/
+
+
+// styled components
+const Container = styled.div`
+  margin: 4rem;
+  height: 100%;
+`
+
+const InnerContainer = styled.div`
+  height: 100%:
+`
+
+const TitleLink = styled(Link)`
+  color: black;
+  text-decoration: none;
+  text-transform: uppercase;
+
+  &:hover {
+    text-decoration: underline;
+  }
+`
+
+const HeaderContainer = styled.div`
+
+`
+
+
+// components
+function Header () {
+  return (
+    <HeaderContainer>
+      <TitleLink to="/">
+          <h1>Boiledss</h1>
+      </TitleLink>
+    </HeaderContainer>
+  )
+}
+
+
+// page component
+const TemplateWrapper = ({ children }) => (
+  <Container>
+    <Helmet
+      title="boiled"
+      meta={[
+        { name: 'description', content: 'Sample' },
+        { name: 'keywords', content: 'sample, something' },
+      ]}
+    />
+
+    <Header />
+
+
+    <ApolloProvider client={client}>
+      {children()}
+    </ApolloProvider>
+
+  </Container>
+)
+
+TemplateWrapper.propTypes = {
+  children: PropTypes.func,
+}
+
+export default TemplateWrapper
+
+
+
+injectGlobal`
+  *, *:before, *:after {
+      box-sizing: border-box;
+      -webkit-overflow-scrolling: touch;
+  }
+
+  html {
+  ${'' /* Maybe Try?  font-size: calc(1.25vw + 62.5%); */}
+    font-size: 62.5%;
+    height: 100%;
+  }
+
+  body {
+      margin: 0;
+      height: 100%;
+
+      font-size: 1.6em;
+      line-height: 1.6;
+      font-weight: 400;
+      font-family: 'Haas Grot Text', 'Helvetica', 'Arial', sans-serif;
+      color: #222;
+      webkit-font-smoothing: antialiased;
+      -moz-osx-font-smoothing: grayscale;
+      -ms-text-size-adjust: 100%;
+      -webkit-text-size-adjust: 100%;
+      text-rendering: optimizeLegibility;
+  }
+
+  #___gatsby {
+    height: 100%;
+  }
+
+  h1, h2, h3, h4, h5, h6 {
+    font-family: 'Haas Grot Display', 'Helvetica', 'Arial', sans-serif;
+    margin-top: 0;
+    margin-bottom: 0rem;
+    font-weight: 400;
+  }
+
+  @font-face {
+    font-family: 'Haas Grot Text';
+    src: url(${NeueHaasGrotTextRoman_eot});
+    src: url('${NeueHaasGrotTextRoman_eot}?#iefix') format('embedded-opentype'),
+      url('${NeueHaasGrotTextRoman_woff2}') format('woff2'),
+      url('${NeueHaasGrotTextRoman_woff}') format('woff');
+    font-weight: 400;
+    font-style: normal;
+    font-stretch: normal;
+  }
+
+  @font-face {
+    font-family: 'Haas Grot Text';
+    src: url(${NeueHaasGrotTextRoman_eot});
+    src: url('${NeueHaasGrotTextItalic_eot}?#iefix') format('embedded-opentype'),
+      url('${NeueHaasGrotTextItalic_woff2}') format('woff2'),
+      url('${NeueHaasGrotTextItalic_woff}') format('woff');
+    font-weight: 400;
+    font-style: italic;
+    font-stretch: normal;
+  }
+
+  @font-face {
+    font-family: 'Haas Grot Display';
+    src: url(${NeueHaasGrotDispRoman_eot});
+    src: url('${NeueHaasGrotDispRoman_eot}?#iefix') format('embedded-opentype'),
+      url('${NeueHaasGrotDispRoman_woff2}') format('woff2'),
+      url('${NeueHaasGrotDispRoman_woff}') format('woff');
+    font-weight: 400;
+    font-style: normal;
+    font-stretch: normal;
+  }
+
+  @font-face {
+    font-family: 'Haas Grot Display';
+    src: url(${NeueHaasGrotDispItalic_eot});
+    src: url('${NeueHaasGrotDispItalic_eot}?#iefix') format('embedded-opentype'),
+      url('${NeueHaasGrotDispItalic_woff2}') format('woff2'),
+      url('${NeueHaasGrotDispItalic_woff}') format('woff');
+    font-weight: 400;
+    font-style: italic;
+    font-stretch: normal;
+  }
+`
